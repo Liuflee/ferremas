48adace566b3e79f86f68994fe1c4a441ddbd700
Angel <118619693+GWhiteWolf@users.noreply.github.com>
2025-05-04 13:57:01 -0400

    cambios interfaz productos

--- tienda/forms.py
+++ tienda/forms.py
@@ -5,3 +5,17 @@ class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
         fields = ['nombre', 'descripcion', 'categoria', 'precio', 'stock', 'imagen']
+        widgets = {
+            'nombre': forms.TextInput(attrs=
+                {'class': 'form-control form-control-sm', 'style': 'max-width: 500px;'}),
+            'descripcion': forms.Textarea(attrs=
+                {'class': 'form-control form-control-sm', 'rows': 3, 'style': 'max-width: 500px;'}),
+            'categoria': forms.TextInput(attrs=
+                {'class': 'form-control form-control-sm', 'style': 'max-width: 500px;'}),
+            'precio': forms.NumberInput(attrs=
+                {'class': 'form-control form-control-sm', 'style': 'max-width: 300px;'}),
+            'stock': forms.NumberInput(attrs=
+                {'class': 'form-control form-control-sm', 'style': 'max-width: 300px;'}),
+            'imagen': forms.ClearableFileInput(attrs=
+                {'class': 'form-control form-control-sm', 'style': 'max-width: 500px;'}),
+        }
